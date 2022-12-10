a76c37977725b4dbe3cef2d074549c54a121eb01
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-10 20:33:56 +0000

    Day 5 Part 2

--- 5/script.py
+++ 5/script.py
@@ -26,10 +26,20 @@ def read_stacks(inputs):
         #print(stacks)
 
 
-def process_move(move, stacks):
+def process_move_single(move, stacks):
     _, num, _, from_, _, to = move.split()
     for crate in range(0, int(num)):
         stacks[int(to)].append(stacks[int(from_)].pop())
+
+
+def process_move_multiple(move, stacks):
+    _, num, _, from_, _, to = move.split()
+    num = int(num)
+    from_ = int(from_)
+    to = int(to)
+    stacks[to].extend(stacks[from_][-num:])
+    print(from_, stacks[from_][-num:], to)
+    del stacks[from_][-num:]
 
 
 with open('input') as f:
@@ -40,7 +50,8 @@ with open('input') as f:
 
     for move in inputs:
         if move.strip():
-            process_move(move, stacks)
+            process_move_multiple(move, stacks)
     #print(stacks)
-    top_crates = [stacks[crate][-1] for crate in range(1,9+1)]
+    top_crates = [stacks[crate][-1] for crate in range(1,9+1) if stacks[crate]]
+    print(top_crates)
     print(''.join(top_crates))
