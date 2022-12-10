e6704f0b787e1c9ec61cdc1a2d95e904f672a5b5
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-10 19:20:01 +0000

    Day 4 Part 2

--- 4/script.py
+++ 4/script.py
@@ -2,6 +2,8 @@ with open('input') as f:
     inputs = (line.rstrip('\n') for line in f)
 
     full_overlaps = 0
+    partial_overlaps = 0
+
     for pair in inputs:
         l, r = pair.split(',')
         ll, lh = map(int, l.split('-'))
@@ -9,6 +11,12 @@ with open('input') as f:
         intervals = sorted([(ll, lh), (rl, rh)], key=lambda rng: rng[1]-rng[0])
         #print(intervals)
         if intervals[0][0] >= intervals[1][0] and intervals[0][1] <= intervals[1][1]:
-            #print('yep')
+            #print('full')
             full_overlaps += 1
+        if not ((intervals[0][0] > intervals[1][1] and intervals[0][1] > intervals[1][1]) or
+            (intervals[0][0] < intervals[1][0] and intervals[0][1] < intervals[1][0])):
+            #print('partial')
+            partial_overlaps += 1
+
     print(full_overlaps)
+    print(partial_overlaps)
