c55eb9c0de877fa0566ed210ff52dc344bba4190
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-12 20:15:50 +0000

    Day 9 Part 2

--- 9/script.py
+++ 9/script.py
@@ -2,8 +2,9 @@ import operator
 
 inputs = (line.rstrip('\n') for line in open('input'))
 
-head = [0,0]
-tail = [0,0]
+segments = []
+for _ in range(0,10):
+    segments.append([0,0])
 
 dirs = {'U': (0,-1),
         'D': (0,1),
@@ -11,7 +12,21 @@ dirs = {'U': (0,-1),
         'R': (1,0)}
 
 unique_tail_coords = set()
-unique_tail_coords.add(tuple(tail))
+unique_tail_coords.add(tuple(segments[-1]))
+
+def move_segment(head, tail):
+    x = head[0]-tail[0]
+    y = head[1]-tail[1]
+    if x in [2,-2] and y in [2,-2]:
+        tail[0] += x//2
+        tail[1] += y//2
+    elif x in [2,-2]:
+        tail[0] += x//2
+        tail[1] = head[1]
+    elif y in [2,-2]:
+        tail[1] += y//2
+        tail[0] = head[0]
+    return tail
 
 for move in inputs:
     direction, repeat = move.split()
@@ -19,16 +34,11 @@ for move in inputs:
     repeat = int(repeat)
 
     while repeat > 0:
-        head = list(map(operator.add, head, d))
-        x = head[0]-tail[0]
-        y = head[1]-tail[1]
-        if x in [2,-2]:
-            tail[0] += x//2
-            tail[1] = head[1]
-        if y in [2,-2]:
-            tail[1] += y//2
-            tail[0] = head[0]
-        unique_tail_coords.add(tuple(tail))
+        segments[0] = list(map(operator.add, segments[0], d))
+        for seg in range(1, len(segments)):
+            segments[seg] = move_segment(segments[seg-1], segments[seg])
+        unique_tail_coords.add(tuple(segments[-1]))
         repeat -= 1
+        #print(move, segments, len(unique_tail_coords))
 
-    print(move, head, tail, len(unique_tail_coords))
+print(len(unique_tail_coords))
