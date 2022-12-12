edb800690cfa622a823e507300ce0dba0bfd21c5
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-12 12:44:04 +0000

    Day 7 Part 2

--- 7/script.py
+++ 7/script.py
@@ -32,18 +32,34 @@ with open('input') as f:
         size = tree_node['size']
         for _, subdir in tree_node['subdirs'].items():
             size += total_size(subdir)
-        tree_node['size'] = size
+        tree_node['total_size'] = size
         return size
 
     total_size(tree)
 
     def sum_sizes(tree_node):
         size_sum = 0
-        if tree_node['size'] <= 100000:
-            size_sum += tree_node['size']
+        if tree_node['total_size'] <= 100000:
+            size_sum += tree_node['total_size']
         for _, subdir in tree_node['subdirs'].items():
             size_sum += sum_sizes(subdir)
         return size_sum
 
     size_sum = sum_sizes(tree)
     print(size_sum)
+
+    total_space = 70000000
+    used_space = tree['total_size']
+    needed_space = 30000000
+    free_space = total_space - used_space
+    to_delete = needed_space - free_space
+
+    def smallest_dir(tree_node, to_delete, smallest_found):
+        for _, subdir in tree_node['subdirs'].items():
+            if smallest_found > subdir['total_size'] > to_delete:
+                smallest_found = subdir['total_size']
+            smallest_found = smallest_dir(subdir, to_delete, smallest_found)
+        return smallest_found
+
+    smallest = smallest_dir(tree, to_delete, tree['total_size'])
+    print(free_space, to_delete, smallest)
