02528b20e9bc52396c12704e9b84defa7cd0f0ff
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-13 23:30:01 +0000

    Day 13 Part 1 (WIP) - Comments

--- 13/script.py
+++ 13/script.py
@@ -20,6 +20,7 @@ def compare(left, right, indent=0):
     print(f'{indent_s(indent)}- Compare {str(left).replace(" ", "")} vs {str(right).replace(" ","")}')
     for i, l in enumerate(left):
         try:
+            # both numbers
             if not isinstance(l, list) and not isinstance(right[i], list):
                 print(f'{indent_s(indent+1)}- Compare {l} vs {right[i]}')
                 if l > right[i]:
@@ -31,6 +32,7 @@ def compare(left, right, indent=0):
                 else:
                     continue
 
+            # left number, right list
             elif not isinstance(l, list) and isinstance(right[i], list):
                 print(f'{indent_s(indent+1)}- Compare {l} vs {str(right[i]).replace(" ","")}')
                 print(f'{indent_s(indent+2)}- Mixed types; convert left to [{l}] and retry comparison')
@@ -38,6 +40,7 @@ def compare(left, right, indent=0):
                 if result in [True, False]:
                     return result
 
+            # left list, right number
             elif isinstance(l, list) and not isinstance(right[i], list):
                 print(f'{indent_s(indent+1)}- Compare {str(l).replace(" ","")} vs {right[i]}')
                 print(f'{indent_s(indent+2)}- Mixed types; convert right to [{right[i]}] and retry comparison')
@@ -45,11 +48,13 @@ def compare(left, right, indent=0):
                 if result in [True, False]:
                     return result
 
+            # both lists
             else:
                 result = compare(l, right[i], indent+1)
                 if result in [True, False]:
                     return result
 
+        # right ran out
         except IndexError:
             print(f'{indent_s(indent+1)}- Right side ran out of items, so inputs are not in the right order')
             return False
