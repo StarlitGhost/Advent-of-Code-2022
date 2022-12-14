1d7602d97a49973be02684ed48ed4cd293a0fb95
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-14 02:43:31 +0000

    Day 13 Part 2

--- 13/script.py
+++ 13/script.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import functools
 
 inputs = (line.rstrip('\n') for line in open(sys.argv[1]))
 
@@ -17,58 +18,61 @@ def indent_s(level):
     return ' '*level*2
 
 def compare(left, right, indent=0):
-    print(f'{indent_s(indent)}- Compare {str(left).replace(" ", "")} vs {str(right).replace(" ","")}')
-    for i, l in enumerate(left):
-        try:
-            # both numbers
-            if not isinstance(l, list) and not isinstance(right[i], list):
-                print(f'{indent_s(indent+1)}- Compare {l} vs {right[i]}')
-                if l > right[i]:
-                    print(f'{indent_s(indent+2)}- Right side is smaller, so inputs are not in the right order')
-                    return False
-                elif l < right[i]:
-                    print(f'{indent_s(indent+2)}- Left side is smaller, so inputs are in the right order')
-                    return True
-                else:
-                    continue
+    #print(f'{indent_s(indent)}- Compare {str(left).replace(" ", "")} vs {str(right).replace(" ","")}')
+    # both numbers
+    if not isinstance(left, list) and not isinstance(right, list):
+        #print(f'{indent_s(indent+1)}- Compare {left} vs {right}')
+        if left > right:
+            #print(f'{indent_s(indent+2)}- Right side is smaller, so inputs are not in the right order')
+            return 1
+        elif left < right:
+            #print(f'{indent_s(indent+2)}- Left side is smaller, so inputs are in the right order')
+            return -1
+        else:
+            return 0
 
-            # left number, right list
-            elif not isinstance(l, list) and isinstance(right[i], list):
-                print(f'{indent_s(indent+1)}- Compare {l} vs {str(right[i]).replace(" ","")}')
-                print(f'{indent_s(indent+2)}- Mixed types; convert left to [{l}] and retry comparison')
-                result = compare([l], right[i], indent+2)
-                if result in [True, False]:
-                    return result
+    # left number, right list
+    elif not isinstance(left, list) and isinstance(right, list):
+        #print(f'{indent_s(indent+1)}- Compare {left} vs {str(right).replace(" ","")}')
+        #print(f'{indent_s(indent+2)}- Mixed types; convert left to [{left}] and retry comparison')
+        return compare([left], right, indent+2)
 
-            # left list, right number
-            elif isinstance(l, list) and not isinstance(right[i], list):
-                print(f'{indent_s(indent+1)}- Compare {str(l).replace(" ","")} vs {right[i]}')
-                print(f'{indent_s(indent+2)}- Mixed types; convert right to [{right[i]}] and retry comparison')
-                result = compare(l, [right[i]], indent+2)
-                if result in [True, False]:
-                    return result
+    # left list, right number
+    elif isinstance(left, list) and not isinstance(right, list):
+        #print(f'{indent_s(indent+1)}- Compare {str(left).replace(" ","")} vs {right}')
+        #print(f'{indent_s(indent+2)}- Mixed types; convert right to [{right}] and retry comparison')
+        return compare(left, [right], indent+2)
 
-            # both lists
-            else:
-                result = compare(l, right[i], indent+1)
-                if result in [True, False]:
-                    return result
+    # both lists, neither empty
+    elif left and right:
+        result = compare(left[0], right[0], indent+1)
+        if result:
+            return result
+        else:
+            return compare(left[1:], right[1:])
 
-        # right ran out
-        except IndexError:
-            print(f'{indent_s(indent+1)}- Right side ran out of items, so inputs are not in the right order')
-            return False
+    # both lists, either/both empty
+    elif left:
+        return 1
+    elif right:
+        return -1
+    else:
+        return 0
 
-    if indent == 0 or len(left) < len(right):
-        print(f'{indent_s(indent+1)}- Left side ran out of items, so inputs are in the right order')
-        return True
 
+packets = [[[2]], [[6]]]
 valid_pairs = []
 for idx, pair in enumerate(pairs):
     one, two = pair
-    print(f'== Pair {idx+1} ==')
-    if compare(one, two) in [True, None]:
+    #print(f'== Pair {idx+1} ==')
+    if compare(one, two) in [-1]:
         valid_pairs.append(idx+1)
     print()
+    packets += [one, two]
 
 print(f'Valid: {valid_pairs} {sum(valid_pairs)}')
+
+sorted_packets = sorted(packets, key=functools.cmp_to_key(compare))
+#for packet in sorted_packets:
+#    print(packet)
+print((sorted_packets.index([[2]]) + 1) * (sorted_packets.index([[6]]) + 1))
