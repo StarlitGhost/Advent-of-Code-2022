a3d9bff3bd9dc2588ea6ec8ff2399401c95cb7d8
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-12-15 17:54:06 +0000

    Day 15 Part 2

--- 15/script.py
+++ 15/script.py
@@ -1,47 +1,9 @@
 import sys
-import math
-
-inputs = (line.rstrip('\n') for line in open(sys.argv[1]))
-
-pairs = []
-
-for line in inputs:
-    sensor, beacon = line.split(': closest beacon is at ')
-    sensor = sensor[10:].split(', y=')
-    sensor = (int(sensor[0][2:]), int(sensor[1]))
-    beacon = beacon[2:].split(', y=')
-    beacon = (int(beacon[0]), int(beacon[1]))
-    pairs.append((sensor, beacon))
 
 def mh_distance(start, end):
     x1, y1 = start
     x2, y2 = end
     return abs(x1 - x2) + abs(y1 - y2)
-
-def dot(left, right):
-    return sum(l*r for l,r in zip(left, right))
-
-def add(left, right):
-    return tuple(l + r for l, r in zip(left, right))
-
-def mul(vec, scalar):
-    return tuple(v * scalar for v in vec)
-
-def to_int(vec):
-    return tuple(int(math.floor(v) if v < 0 else math.ceil(v)) for v in vec)
-
-def ray_circle_intersection(ray_start, ray_dir, circle_center, circle_radius):
-    #print(circle_center, circle_radius)
-
-    L = (circle_center[0] - ray_start[0], circle_center[1] - ray_start[1])
-    tca = dot(L, ray_dir)
-    d2 = dot(L, L) - tca**2
-    if d2 > circle_radius**2:
-        return None
-    thc = math.sqrt(circle_radius**2 - d2)
-    t0 = add(ray_start, mul(ray_dir, tca - thc))
-    t1 = add(ray_start, mul(ray_dir, tca + thc))
-    return t0, t1
 
 def clamp1(n):
     return max(min(n, 1), -1)
@@ -62,25 +24,63 @@ def walk(start, end):
         y += dy
     return points
 
-covered = set()
-beacons = set(beacon for _, beacon in pairs)
-for sensor, beacon in pairs:
-    ray_start = (0,int(sys.argv[2]))
-    ray_dir = (1,0)
-    radius = mh_distance(sensor, beacon)+1
-    intersects = ray_circle_intersection(ray_start, ray_dir, sensor, radius)
-    if intersects:
-        #print(intersects)
-        t0, t1 = intersects
-        t0 = to_int(t0)
-        t1 = to_int(t1)
-        #print((t0, t1))
-        points = walk(t0, t1)
-        #print(points)
-        for point in points:
-            if mh_distance(sensor, point) < radius:
-                covered.add(point)
+def row_sensor_intersection(row, sensor, mh_radius):
+    if sensor[1]-mh_radius <= row <= sensor[1]+mh_radius:
+        y_distance = abs(sensor[1]-row)
+        depth = mh_radius - y_distance
+        start_x = sensor[0] - depth
+        end_x = sensor[0] + depth
+        return (start_x, row), (end_x, row)
+    return None
 
-covered -= beacons
+def scan(row, pairs):
+    intersections = []
+    for sensor, beacon in pairs:
+        radius = mh_distance(sensor, beacon)
+        intersects = row_sensor_intersection(row, sensor, radius)
+        if intersects:
+            t0, t1 = intersects
+            intersections.append(intersects)
+    return intersections
 
-print(len(covered))#, covered)
+def row_coverage(intersections):
+    covered = set()
+    for i in intersections:
+        points = walk(i[0], i[1])
+        covered.update(points)
+    return covered
+
+if __name__ == '__main__':
+    inputs = (line.rstrip('\n') for line in open(sys.argv[1]))
+
+    pairs = []
+
+    for line in inputs:
+        sensor, beacon = line.split(': closest beacon is at ')
+        sensor = sensor[10:].split(', y=')
+        sensor = (int(sensor[0][2:]), int(sensor[1]))
+        beacon = beacon[2:].split(', y=')
+        beacon = (int(beacon[0]), int(beacon[1]))
+        pairs.append((sensor, beacon))
+
+    beacons = set(b for _, b in pairs)
+    intersections = scan(int(sys.argv[2]), pairs)
+    covered = row_coverage(intersections)
+    covered -= beacons
+
+    print('part 1:', len(covered))
+
+    max_coord = int(sys.argv[3])
+
+    for y in range(0,max_coord+1):
+        intersections = scan(y, pairs)
+        x = 0
+        while x <= max_coord:
+            no_intersections = True
+            for i in intersections:
+                if i[0][0] <= x <= i[1][0]:
+                    no_intersections = False
+                    x = i[1][0] + 1
+            if no_intersections:
+                print('part 2:', (x,y), x*4000000 + y)
+                exit()
